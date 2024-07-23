272b26b04109b79a40eb90e3fb1fe0ac782586f8
Ashu23052002 <ans23052002@gmail.com>
2024-07-23 22:07:55 +0530

    final done

---
+++ src/app/(app)/layout.tsx
@@ -0,0 +1,21 @@
+
+
+import { SessionProvider } from "next-auth/react";
+import Navbar from "@/components/Navbar";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        <SessionProvider>
+          <Navbar />
+          {children}
+        </SessionProvider>
+      </body>
+    </html>
+  );
+}
--- src/app/u/[username]/page.tsx
+++ src/app/u/[username]/page.tsx
@@ -1,12 +1,8 @@
-import Navbar from '@/components/Navbar'
 import React from 'react'
 
 const page = () => {
   return (
-    <>
-    <Navbar />
     <div>page</div>
-    </>
   )
 }
 
